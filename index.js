d53bad93e8bf6a4b27fa43999945adcfd1bddd66
zaw z tun <americanarakan@gmail.com>
2019-09-03 06:30:20 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ var line = [];
 function currentLine (katzDeli) {
   for ( var i = 0 ; i < katzDeli.length ; i ++);
   line.push(` ` +[i +1]+`. ` + katzDeli[i])
-  if (katzDeli.length === 0)
+  if (katzDeli.length === 0){
   return `The line is currently empty.`;
 } else
 return (`The line is currently:` + line);
