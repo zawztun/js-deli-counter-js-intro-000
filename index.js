6b23163a737996edf49680cdb3978bd3811ad6fe
zaw z tun <americanarakan@gmail.com>
2019-09-03 06:34:35 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ katzDeliLine.push(name)
 }
 
 function nowServing (katzDeli){
-  for(var i = 0; i < katzDeli.length; i++);
+  for(var i = 0; i < katzDeli.length; i++);{
   if (katzDeli.length === 0){
       return `There is nobody waiting to be served!`
   }else
@@ -19,9 +19,11 @@ var line = [];
 function currentLine (katzDeli) {
   for ( var i = 0 ; i < katzDeli.length ; i ++);
   line.push(` ` +[i +1]+`. ` + katzDeli[i])
+}
   if (katzDeli.length === 0){
   return `The line is currently empty.`;
-} else
+}
+else
 return (`The line is currently:` + line);
 
 }
