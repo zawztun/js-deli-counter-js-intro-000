6d08ffab75ff228b526353ced7329a977fafb6a1
zaw z tun <americanarakan@gmail.com>
2019-09-03 06:11:27 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -14,6 +14,12 @@ function nowServing (katzDeli){
       return ( `Currently serving ${katzDeli.shift()}.`)
 }
 
+function currentLine (katzDeli) {
+  if (katzDeli.length === 0)
+  return `The line is currently empty.`
+}
+
+
 
 
 
