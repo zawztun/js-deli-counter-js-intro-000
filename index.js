2f6cad30d67b79ad32cc0ece24a54a51c0f3c314
zaw z tun <americanarakan@gmail.com>
2019-09-03 06:49:49 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 var katzDeliLine = [];
+var number = 0;
 function takeANumber(katzDeliLine, name){
 katzDeliLine.push(name)
   return `Welcome, ${name}. You are number ${katzDeliLine.length} in line.`
