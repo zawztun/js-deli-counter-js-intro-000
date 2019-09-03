2964d70aea2d59108ff24bd2d56ea824ed1d8a06
zaw z tun <americanarakan@gmail.com>
2019-09-03 06:08:10 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ function nowServing (katzDeli){
   if (katzDeli.length === 0){
       return `There is nobody waiting to be served!`
   }else
-      return ( `Currently serving ${katzDeli.shift()}.`)
+      return ( `Now serving ${katzDeli.shift()}.`)
 }
 
 
